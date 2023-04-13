4254b411612c2030638b0197b6c0ddf2a3f8cb2a
ejrtn <dlstn153@naver.com>
2023-04-13 11:32:55 +0900

    first commit

---
+++ src/main/java/org/example/Child3Application.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Child3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Child3Application.class, args);
+    }
+}
