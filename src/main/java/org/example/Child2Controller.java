5763aeea6ca778ebd1ecf24eb372316bfe0149cc
ejrtn <dlstn153@naver.com>
2023-04-13 11:32:14 +0900

    first commit

---
+++ src/main/java/org/example/Child2Controller.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Child2Controller {
+    public String main(){
+        return "main.html";
+    }
+}
